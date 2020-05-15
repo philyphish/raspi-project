2625d1bdc7642df132137b1fe5b846924e16250d
philyphish <philipknigh@gmail.com>
2020-05-15 13:38:30 -0500

    Client is hosted by express

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,27 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import * as createError from 'http-errors';
+import * as express from 'express';
+import * as path from 'path';
+import * as cookieParser from 'cookie-parser';
+import * as logger from 'morgan';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const app = express();
+const port = 3000;
+const clientPath = 'dist/src/client';
+const indexFilePath = path.join(clientPath, 'index.html');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(clientPath));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
+// Client Path
+app.get('/*', (req, res) => {
+  res.sendFile(indexFilePath, { root: './' },)});
+
+  app.listen(port, () => {
+    console.log('Listening on port ' + port);
+  })
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
