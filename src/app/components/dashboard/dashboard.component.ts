b02d34103a81980b26adf75bd38f6cc7c0edbb6c
philyphish <philipknigh@gmail.com>
2020-05-17 09:25:30 -0500

    Moves calls to store to service

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Store } from '@ngrx/store';
 import * as gpioActions from 'src/app/store/gpio.actions';
+import { SwitchService } from '../../services/switch.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,23 +12,27 @@ import * as gpioActions from 'src/app/store/gpio.actions';
 export class DashboardComponent implements OnInit {
   public led: string;
 
-  constructor(private store: Store<any>) { }
+  constructor(private store: Store<any>, private switchService: SwitchService) { }
 
   ngOnInit() {
   }
 
   public ledSwitch() {
-    let gpioSwitch: string;
-
-    this.store.select('SendMessage')
-      .subscribe(state => (gpioSwitch = state.message)
-    );
-    console.log('GPIO SWITCH: ', gpioSwitch);
-    gpioSwitch === 'off' ? gpioSwitch = 'on' : gpioSwitch = 'off';
-
-    this.store.dispatch(new gpioActions.SendMessage(gpioSwitch));
-
-    console.log(`turn ${gpioSwitch} led!`);
-    this.led = gpioSwitch;
+    this.led = this.switchService.ledSwitch();
   }
+
+  // public ledSwitch() {
+  //   let gpioSwitch: string;
+
+  //   this.store.select('SendMessage')
+  //     .subscribe(state => (gpioSwitch = state.message)
+  //   );
+  //   console.log('GPIO SWITCH: ', gpioSwitch);
+  //   gpioSwitch === 'off' ? gpioSwitch = 'on' : gpioSwitch = 'off';
+
+  //   this.store.dispatch(new gpioActions.SendMessage(gpioSwitch));
+
+  //   console.log(`turn ${gpioSwitch} led!`);
+  //   this.led = gpioSwitch;
+  // }
 }
---
+++ src/app/services/switch.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import * as gpioActions from '../store/gpio.actions';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SwitchService {
+
+  constructor(private store: Store<any>) { }
+
+  public ledSwitch() {
+    let gpioSwitch: string;
+
+    this.store.select('SendMessage')
+      .subscribe(state => (gpioSwitch = state.message)
+    );
+    console.log('GPIO SWITCH: ', gpioSwitch);
+    gpioSwitch === 'off' ? gpioSwitch = 'on' : gpioSwitch = 'off';
+
+    this.store.dispatch(new gpioActions.SendMessage(gpioSwitch));
+
+    console.log(`turn ${gpioSwitch} led!`);
+    return gpioSwitch;
+  }
+
+}
